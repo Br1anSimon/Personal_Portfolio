d964c9c4b038cfd939ad98ab6b07791938af6fad
DiamondShadows <bsimon2005@gmail.com>
2023-08-11 02:13:37 -0400

    Iteration 3

---
+++ JS/Main.js
@@ -0,0 +1,11 @@
+function techstackbuttonfunction() {
+    document.querySelector(".toolsbutton").style.background="rgba(0,70,71,0.0)";
+    document.querySelector(".techstackbutton").style.background="rgba(0,70,71,0.6)";
+
+}
+
+
+function toolsbuttonfunction(){
+    document.querySelector(".techstackbutton").style.background="rgba(0,70,71,0)"
+    document.querySelector(".toolsbutton").style.background="rgba(0,70,71,0.6)";
+}
