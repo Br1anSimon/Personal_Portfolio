e68dcfba20ede76dacfb527c951cd474d8420052
DiamondShadows <bsimon2005@gmail.com>
2023-08-11 17:25:10 -0400

    Added Fonts Directory

--- JS/Main.js
+++ JS/Main.js
@@ -1,11 +1,30 @@
 function techstackbuttonfunction() {
     document.querySelector(".toolsbutton").style.background="rgba(0,70,71,0.0)";
     document.querySelector(".techstackbutton").style.background="rgba(0,70,71,0.6)";
-
+    document.getElementById("title1").innerHTML=("HTML")
+    document.getElementById("img1").innerHTML=(" <img src=\"../IMAGES/htmlimage.png\" width=\"40px\" height=\"40px\">")
+    document.getElementById("level1").innerHTML=("Intermediate");
+    document.getElementById("title2").innerHTML=("CSS")
+    document.getElementById("img2").innerHTML=(" <img src=\"../IMAGES/cssimage.png\" width=\"40px\" height=\"40px\">")
+    document.getElementById("level2").innerHTML=("Intermediate");
+    document.getElementById("title3").innerHTML=("Javascript")
+    document.getElementById("img3").innerHTML=(" <img src=\"../IMAGES/javascriptimage.png\" width=\"40px\" height=\"40px\">")
+    document.getElementById("level3").innerHTML=("Intermediate");
+    document.getElementById("title4").innerHTML=("Python")
+    document.getElementById("img4").innerHTML=(" <img src=\"../IMAGES/pythonimage.png\" width=\"40px\" height=\"40px\">")
+    document.getElementById("level4").innerHTML=("Intermediate");
 }
 
 
 function toolsbuttonfunction(){
     document.querySelector(".techstackbutton").style.background="rgba(0,70,71,0)"
     document.querySelector(".toolsbutton").style.background="rgba(0,70,71,0.6)";
+    document.getElementById("title1").innerHTML=("Git");
+    document.getElementById("img1").innerHTML=(" <img src=\"../IMAGES/gitimage.png\" width=\"40px\" height=\"40px\">");
+    document.getElementById("level1").innerHTML=("Version Control");
+    document.getElementById("title2").innerHTML=("Github");
+    document.getElementById("img2").innerHTML=(" <img src=\"../IMAGES/Github%20Logo.PNG\" width=\"40px\" height=\"40px\">");
+    document.getElementById("level2").innerHTML=("Git Hosting");
+
+
 }
